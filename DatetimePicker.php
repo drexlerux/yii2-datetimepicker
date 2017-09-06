cd746a3268548f61e8c94344d25a2897f5a2a131
Carlos Porras <drexlerux@gmail.com>
2017-09-06 11:35:35 -0500

    Initial Commit

---
+++ DatetimePicker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace drexlerux\DatetimePicker;
+
+/**
+ * This is just an example.
+ */
+class DatetimePicker extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
