151160652079172847a7d0aea4268eb2db72cd7d
Carlos Porras <drexlerux@gmail.com>
2017-09-06 21:09:40 -0500

    Register assets

---
+++ DateTimePickerAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace drexlerux\datetimepicker;
+use yii\web\AssetBundle;
+/**
+ * LcsWitch AssetBundle class for call lc switch bower-asset
+ */
+
+class DateTimePickerAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/flat-datetimepicker';
+    public $js = [
+        'dist/DateTimePicker.min.js',
+        'dist/i18n/DateTimePicker-i18n.js'
+    ];
+
+    public $css = [
+        'dist/DateTimePicker.min.css',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+}
--- DatetimePicker.php
+++ DatetimePicker.php
@@ -1,14 +1,50 @@
 <?php
 
-namespace drexlerux\DatetimePicker;
-
+namespace drexlerux\datetimepicker;
+use Yii;
+use yii\bootstrap\Html;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
 /**
  * This is just an example.
  */
 class DatetimePicker extends \yii\base\Widget
 {
+    public $options = [];
+    public $widgetOptions = [];
+    private $inputId;
+    public function init()
+    {
+        $defaults = [
+            'data-field' => 'date',
+            'readonly' => true
+        ];
+
+        $this->options = array_merge($defaults, $this->options);
+
+        $this->inputId = Html::getInputId($this->model, $this->attribute);
+
+        $this->widgetOptions = Json::encode($this->widgetOptions);
+    }
+
     public function run()
     {
-        return "Hello!";
+        $this->registerScript();
+        $input = Html::activeTextInput($this->model, $this->attribute, $this->options);
+        $box = "<div id=\"$this->inputId\"></div>"
+        echo $input . $box;
+    }
+
+    public function registerScript(){
+        $view = $this->getView();
+        $asset = DateTimePickerAsset::register($view);
+
+        $js =  "
+            $(document).ready(function(){
+                $('#$this->inputId').DateTimePicker($this->widgetOptions);
+            });
+        ";
+
+        $view->registerJs($js);
     }
 }
